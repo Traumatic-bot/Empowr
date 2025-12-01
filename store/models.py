23f1dc2df87b7864c942f5fbca1c66ab35395a44
Irfan <irfan06ahmed@gmail.com>
2025-12-01 00:53:50 +0000

    Developed user login and signup data and auth

--- store/models.py
+++ store/models.py
@@ -2,18 +2,16 @@ from django.db import models
 
 
 class Customer(models.Model):
-    # matches: users table (first_name, last_name, email, password)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email = models.EmailField(unique=True)
-    password = models.CharField(max_length=255)  # later you can switch to Django auth
+    password = models.CharField(max_length=255) 
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
 
 
 class Category(models.Model):
-
     name = models.CharField(max_length=100)
 
     def __str__(self):
@@ -21,12 +19,14 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-
     product_name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock_quantity = models.IntegerField(default=0)
     category = models.ForeignKey(
-        Category, on_delete=models.SET_NULL, null=True, blank=True
+        Category,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
     )
     description = models.TextField(blank=True, null=True)
 
@@ -35,7 +35,6 @@ class Product(models.Model):
 
 
 class CartItem(models.Model):
-
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.IntegerField(default=1)
@@ -43,7 +42,6 @@ class CartItem(models.Model):
     total_price = models.DecimalField(max_digits=10, decimal_places=2)
 
     def save(self, *args, **kwargs):
-
         self.total_price = self.quantity * self.unit_price
         super().save(*args, **kwargs)
 
