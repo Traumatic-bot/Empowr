21c70fa6ca0984781a5f3017d20c7e97d78044fd
Joe <240111586@aston.ac.uk>
2025-12-04 09:43:40 +0000

    Delete store directory

--- store/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Customer, Category, Product, CheckoutItem
-
-admin.site.register(Customer)
-admin.site.register(Category)
-admin.site.register(Product)
-admin.site.register(CheckoutItem)
