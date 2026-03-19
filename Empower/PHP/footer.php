30f7759b8007ef0c82c9ef9d396b21c06952be2b
ctahmid <tahmidhc@gmail.com>
2026-03-19 12:42:57 +0000

    Fixed the footer

--- Empower/PHP/footer.php
+++ Empower/PHP/footer.php
@@ -1,7 +1,7 @@
 <footer>
     <div class="footer-links" style="color: black;">
         <a href="terms_conditions.php" style="color: black; text-decoration: none;">Terms & Conditions</a> ｜
-        <a href="privacy_policy.php" style="color: black; text-decoration: none;">Privacy Policy</a> ｜
+        <a href="privacy_policy.php" style="color: black; text-decoration: none;">Privacy Policy</a>
     </div>
 </footer>
 
